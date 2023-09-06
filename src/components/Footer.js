328263f7232dfdfb802692d308b762129e2a65d9
Edison611 <edisony611@gmail.com>
2023-09-06 18:58:57 -0400

    fix : footer positioning

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,12 @@ import '../component-styles/Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <footer>
-                <p>&copy; {new Date().getFullYear()} iNNOVATIVE. All rights reserved.</p>
-                <hr />
-                <p>Made by Edison Ying, Haris Khawja, Roy Zhang</p>
-                <p>Email: <a href="mailto: jfss.innovate@gmail.com">jfss.innovate@gmail.com</a></p>
-            </footer>
-        </div>
+        <footer>
+            <p>&copy; {new Date().getFullYear()} iNNOVATIVE. All rights reserved.</p>
+            <hr />
+            <p>Made by Edison Ying, Haris Khawja, Roy Zhang</p>
+            <p>Email: <a href="mailto: jfss.innovate@gmail.com">jfss.innovate@gmail.com</a></p>
+        </footer>
     )
 }
 
