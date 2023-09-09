c13f6204136dcbcf97d23513b8138534e7ce3db6
Edison611 <edisony611@gmail.com>
2023-09-09 18:15:34 -0400

    test database

--- src/pages/Forum.js
+++ src/pages/Forum.js
@@ -1,59 +1,120 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import '../page-styles/Forum.css';
 
-function read_csv(){
-    const csv = require('csv-parser');
-    const fs = require('fs');
+function openDatabase() {
+  return new Promise((resolve, reject) => {
+    const request = window.indexedDB.open('CommentsDatabase', 1);
 
-    fs.createReadStream('.\\src\\pages\\comments.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-        console.log(row);
-        console.log(row["Date"])
-    })
-    .on('end', () => {
-        console.log('CSV file successfully processed');
-    });
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
 
+      if (!db.objectStoreNames.contains('comments')) {
+        db.createObjectStore('comments', { keyPath: 'id', autoIncrement: true });
+      }
+    };
+
+    request.onsuccess = (event) => {
+      resolve(event.target.result);
+    };
+
+    request.onerror = (event) => {
+      reject(event.target.error);
+    };
+  });
 }
-
-/* ID,Name,Date,ChildID,ParentID,PFP,Comment */ 
-/* 1,Roy, 2023-09-09 14:09:00,,,,test */ 
-class Comment {
-    constructor(id = "", username="", date="", child_id="", parent_id="", pfp="", comment="") {
-        this.id = id;
-        this.username = username;
-        this.date = date;
-        this.child_id = child_id;
-        this.parent_id = parent_id;
-        this.pfp = pfp;
-        this.comment = comment;
-    }
-
-    saveAsCSV() {
-        const csv = `${this.id},${this.username},${this.date},${this.date},${this.child_id},${this.parent_id},${this.pfp},${this.comment}\n`;
-        try {
-            appendFileSync('.\\src\\pages\\comments.csv', csv);
-        } catch (err) {
-            console.error(err);
-        }
-    }
-}
-
 
 const Forum = () => {
-    comments = read_csv();
-    const row = new Comment("1", "2", "", "", "","","","");
-    row.saveAsCSV();
-    return(
-        <div className="main-container">
-            <h1 className="text">Welcome to the forum! Here you can ask questions, share your ideas, or simply just chat!</h1>
-            <div>
-                <input placeholder="Add a comment..." className="comment-box"></input>
-                <button className="comment-button">Comment</button>
+  const [comments, setComments] = useState([]);
+  const [commentText, setCommentText] = useState('');
+  const [commentName, setCommentName] = useState('Your Name'); // Default username
+
+  useEffect(() => {
+    openDatabase()
+      .then((db) => {
+        const transaction = db.transaction('comments', 'readonly');
+        const objectStore = transaction.objectStore('comments');
+
+        const request = objectStore.getAll();
+
+        request.onsuccess = () => {
+          setComments(request.result);
+        };
+
+        request.onerror = (event) => {
+          console.error('Error fetching comments:', event.target.error);
+        };
+      })
+      .catch((error) => {
+        console.error('Error opening database:', error);
+      });
+  }, []);
+
+  const saveComment = (comment) => {
+    openDatabase()
+      .then((db) => {
+        const transaction = db.transaction('comments', 'readwrite');
+        const objectStore = transaction.objectStore('comments');
+
+        const request = objectStore.add(comment);
+
+        request.onsuccess = () => {
+          setComments([...comments, comment]);
+        };
+
+        request.onerror = (event) => {
+          console.error('Error adding comment:', event.target.error);
+        };
+      })
+      .catch((error) => {
+        console.error('Error opening database:', error);
+      });
+  };
+
+  const handleAddComment = () => {
+    const newComment = {
+      username: commentName,
+      date: new Date().toLocaleString(),
+      comment: commentText,
+    };
+
+    saveComment(newComment);
+    setCommentText('');
+  };
+
+  return (
+    <div className="main-container">
+      <h1 className="text">
+        Welcome to the forum! Here you can ask questions, share your ideas, or simply just chat!
+      </h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Your Name"
+          value={commentName}
+          onChange={(e) => setCommentName(e.target.value)}
+        />
+        <textarea
+          placeholder="Add a comment..."
+          value={commentText}
+          onChange={(e) => setCommentText(e.target.value)}
+        />
+        <button className="comment-button" onClick={handleAddComment}>
+          Comment
+        </button>
+        <div>
+          {comments.map((comment, index) => (
+            <div key={index} className="comment">
+              <div className="comment-info">
+                <span className="comment-username">{comment.username}</span>
+                <span className="comment-date">{comment.date}</span>
+              </div>
+              <div className="comment-text">{comment.comment}</div>
             </div>
+          ))}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Forum;
