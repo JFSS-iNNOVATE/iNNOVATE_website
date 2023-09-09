1caf7dc39f42f74178bd4ec553e69880d637e7de
Haris Khawja <92524990+hariskhawja@users.noreply.github.com>
2023-09-09 16:09:31 -0400

    feat: footer additions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
             <p>&copy; {new Date().getFullYear()} iNNOVATIVE. All rights reserved.</p>
             <hr />
             <p>Made by Edison Ying, <a href="https://hariskhawja.github.io/">Haris Khawja</a>, Roy Zhang</p>
+            <p>Powered by <a href="https://johnfraser.peelschools.org/">John Fraser Secondary School</a>, Sponsored by JFSS Cubing Club</p>
             <p>Email: <a href="mailto: jfss.innovate@gmail.com">jfss.innovate@gmail.com</a></p>
         </footer>
     )
