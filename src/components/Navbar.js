405a229e91b970d7944e4f87505af7f28aee8821
Haris Khawja <92524990+hariskhawja@users.noreply.github.com>
2023-09-10 18:48:57 -0400

    feat: hamburger toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,19 @@ import iNNOVATELogo from '../images/iNNOVATELogo.png'
 
 var userObject = {}
 const Navbar = () => {
+    /*function renderGoogle(){
+        google.accounts.id.renderButton(
+            document.getElementById("LoginDiv"), 
+            {theme: "outline", size: "large"}
+        );
+    }*/
+
+    //renderGoogle();
+
+    const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 900);
     const [user, setUser] = useState({});
+    const [toggled, setToggled] = useState(0);
+
     function handleCallback(response) {
         userObject = jwt_decode(response.credential)
         console.log(userObject);
@@ -21,7 +33,25 @@ const Navbar = () => {
         document.getElementById("LoginDiv").hidden = false;
     }
 
+    function navToggle() {
+        if (toggled == 0) {
+            setToggled(1);
+        }
+
+        else {
+            setToggled(0);
+        }
+    }
+    
+    function hamburger() {
+        // renderGoogle();
+        navToggle();
+    }
+
     useEffect(() => {
+        if (user.name) {
+            return;
+        }
         /* global google */
         google.accounts.id.initialize({
             client_id: "680968852601-6h8k53sd636mg9hfu2eqmvs8vjtg8skj.apps.googleusercontent.com",
@@ -34,11 +64,42 @@ const Navbar = () => {
         );
 
         google.accounts.id.prompt();
-    }, []);
+
+        const handleResize = () => {
+            setIsSmallScreen(window.innerWidth <= 900);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+        window.removeEventListener('resize', handleResize);
+        };
+
+    }, [toggled]);
     
     return (
         <nav className="nav">
             <div className="logoContainer"><Link to="/"><img className="logo" src={iNNOVATELogo}/></Link></div>
+            {isSmallScreen ? (
+                <ul className="menu">
+                <li><button onClick={hamburger}>☰</button></li>
+                {toggled == 0 && 
+                <li><ul className="menuList">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/Resources">Resources</Link></li>
+                <li><Link to="/Forum">Forums</Link></li>
+                <li><div className="google-login" id="LoginDiv"></div></li>
+                <li>{user.picture &&
+                    <div>
+                       <img src={user.picture} className="pfp"></img>
+                    </div>
+                }</li>
+                <li>{Object.keys(user).length != 0 &&
+                    <li><button onClick={ () => signOut()} className="sign-out" id="pfp">Sign Out</button></li>
+                }</li>
+            </ul></li>}
+            </ul>
+            ) : (
             <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/Resources">Resources</Link></li>
@@ -53,6 +114,7 @@ const Navbar = () => {
                     <li><button onClick={ () => signOut()} className="sign-out" id="pfp">Sign Out</button></li>
                 }</li>
             </ul>
+            )}
         </nav>
     )
 }
