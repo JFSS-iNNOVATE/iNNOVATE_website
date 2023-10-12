64cd4da0beece494d54221721a08bac042e30cff
RoyZ1017 <99610201+RoyZ1017@users.noreply.github.com>
2023-10-12 16:22:16 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <p className="firstFooter">&copy; {new Date().getFullYear()} iNNOVATE. All rights reserved.</p>
             <hr />
             <p>Made by Edison Ying, <a href="https://hariskhawja.github.io/">Haris Khawja</a>, Roy Zhang</p>
-            <p>Powered by <a href="https://johnfraser.peelschools.org/">John Fraser Secondary School</a>, Sponsored by JFSS Cubing Club</p>
+            <p>Powered by <a href="https://johnfraser.peelschools.org/">John Fraser Secondary School</a></p>
             <p>Contact/Email: <a href="mailto: jfss.innovate@gmail.com">jfss.innovate@gmail.com</a></p>
         </footer>
     )
